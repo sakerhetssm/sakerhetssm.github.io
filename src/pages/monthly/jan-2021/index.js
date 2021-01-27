67408055a27fa435bca84882b77cc3b8360a817f
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2021-01-27 17:35:27 +0100

    hint jan-2021

--- src/pages/monthly/jan-2021/index.js
+++ src/pages/monthly/jan-2021/index.js
@@ -6,6 +6,10 @@ const Jan2021Page = () => {
     <MonthlyLayout>
       <h1>Månadens problem - Januari 2021</h1>
       <p>ACME Corp har haft en... dataincident och vill nu anlita dig för att lista ut vad som hände. De har hittat lite spår och undrar vilken data som förövarna har lyckats exfiltrera. <a href="/monthly/jan-2021/chall.zip">Fil</a>.</p>
+      <details>
+        <summary>Hint</summary>
+        xor
+      </details>
     </MonthlyLayout>
   )
 }
