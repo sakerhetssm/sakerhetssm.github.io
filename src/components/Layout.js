9ae8c63d8ed3aeb7b955f1ff004a6466836bae70
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2021-02-15 21:38:51 +0100

    removed seo from layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,10 @@ import * as React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import style from "../styles/Layout.module.scss"
-import SEO from "./SEO"
 
 const Layout = ({ children, navbar }) => {
   return (
     <div className={style.container}>
-      <SEO />
       <Header />
       {navbar}
       <div className={style.area}>
