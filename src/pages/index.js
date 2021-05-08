a56a68bb3782abd644ac90f97e602e00d7b34e93
Mattias Grenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2021-05-08 10:26:36 +0200

    finaldatum

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => {
       <ul>
         <li>Träningstävling: 15 mars - 21 mars</li>
         <li>Kvalet: 20:00, 26 mars - 20:00, 28 mars, 47h</li>
-        <li>Finalen: maj eller juni</li>
+        <li>Finalen: 12:00, 29 maj - 12:00, 30 maj, 24h</li>
       </ul>
 
       <h1>Lös första problemet redan nu!</h1>
