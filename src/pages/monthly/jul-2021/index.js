919542aea26fa1ff7025045822c05fc7e64d03d9
mateuszdrwal <drwal.mateusz@gmail.com>
2021-07-06 23:54:10 +0200

    jul-2021

---
+++ src/pages/monthly/jul-2021/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import MonthlyLayout from "../../../components/MonthlyLayout"
+
+const Jul2021Page = () => {
+  return (
+    <MonthlyLayout>
+      <h1>Månadens problem - Juli 2021</h1>
+      <p>Sedan förra månadens PHP-fadäs har vi på Kodsport insett att vi måsta använda enterprise-teknologi från och med nu. Äntligen kan ingen komma åt våra flaggor! <a href="http://35.228.72.74:8010/">Länk</a>.</p>
+    </MonthlyLayout>
+  )
+}
+
+export default Jul2021Page
