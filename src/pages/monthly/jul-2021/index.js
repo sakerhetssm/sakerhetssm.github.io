5721b55a05c36c4a835b7094c8ad5caf1bd3e37f
mateuszdrwal <drwal.mateusz@gmail.com>
2021-07-31 20:49:19 +0200

    jul-2021 hint

--- src/pages/monthly/jul-2021/index.js
+++ src/pages/monthly/jul-2021/index.js
@@ -6,6 +6,13 @@ const Jul2021Page = () => {
     <MonthlyLayout>
       <h1>Månadens problem - Juli 2021</h1>
       <p>Sedan förra månadens PHP-fadäs har vi på Kodsport insett att vi måsta använda enterprise-teknologi från och med nu. Äntligen kan ingen komma åt våra flaggor! <a href="http://35.228.72.74:8010/">Länk</a>.</p>
+
+      <details>
+        <summary>
+          Hint
+        </summary>
+        <p>Eclipse MAT är ett bra program för felsökning. Inte för att vi har buggar eller så…</p>
+      </details>
     </MonthlyLayout>
   )
 }
