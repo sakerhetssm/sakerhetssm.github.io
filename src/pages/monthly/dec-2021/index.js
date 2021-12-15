adfd8b8d54912af533b22f39c4144f46211de079
mateuszdrwal <drwal.mateusz@gmail.com>
2021-12-15 14:36:07 +0100

    dec 2021

---
+++ src/pages/monthly/dec-2021/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import MonthlyLayout from "../../../components/MonthlyLayout"
+
+const Apr2021Page = () => {
+  return (
+    <MonthlyLayout>
+      <h1>Månadens problem - December 2021</h1>
+      <p>Tomten kommer tidigt och har redan gett dig LFI på en debian-server, men kan du <i>lokalisera</i> flaggan? <a href="http://35.228.72.74:8012/">Länk</a>.</p>
+
+      {/* <details>
+        <summary>
+          Hint
+        </summary>
+        <p>...</p>
+      </details> */}
+    </MonthlyLayout>
+  )
+}
+
+export default Apr2021Page
