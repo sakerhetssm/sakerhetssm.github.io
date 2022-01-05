6ab7c8b46c90b3974fca783b3eef1c4d93e90ea4
mateuszdrwal <drwal.mateusz@gmail.com>
2022-01-05 23:38:47 +0100

    dec-2021 hint och jan-2022

--- src/pages/monthly/dec-2021/index.js
+++ src/pages/monthly/dec-2021/index.js
@@ -7,12 +7,16 @@ const Apr2021Page = () => {
       <h1>Månadens problem - December 2021</h1>
       <p>Tomten kommer tidigt och har redan gett dig LFI på en debian-server, men kan du <i>lokalisera</i> flaggan? <a href="http://35.228.72.74:8012/">Länk</a>.</p>
 
-      {/* <details>
+      <details>
         <summary>
           Hint
         </summary>
-        <p>...</p>
-      </details> */}
+        <pre>1. Utmaningen är endast LFI, dvs ingen exploatering behövs
+2. Flaggan finns i en fil på en plats som inte går att gissa sig till
+3. Du behöver inte gå via /proc eller liknande
+4. Att servern kör Debian har viss signifikans.
+5. Hur skulle du <i>lokalisera</i> filen, om det var på din dator?</pre>
+      </details>
     </MonthlyLayout>
   )
 }
---
+++ src/pages/monthly/jan-2022/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import MonthlyLayout from "../../../components/MonthlyLayout"
+
+const Apr2021Page = () => {
+  return (
+    <MonthlyLayout>
+      <h1>Månadens problem - Januari 2022</h1>
+      <p>Onej, nåt har hänt med min fina flagga! Hjälp! <a href="/monthly/jan-2022/chall.gif">Fil</a>.</p>
+
+      {/* <details>
+        <summary>
+          Hint
+        </summary>
+        <p>...</p>
+      </details> */}
+    </MonthlyLayout>
+  )
+}
+
+export default Apr2021Page
