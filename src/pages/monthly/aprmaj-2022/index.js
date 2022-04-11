879467f16babad07f2d5ae85de94e4b3a6e19645
mateuszdrwal <drwal.mateusz@gmail.com>
2022-04-11 23:06:05 +0200

    uhhh

--- src/pages/monthly/aprmaj-2022/index.js
+++ src/pages/monthly/aprmaj-2022/index.js
@@ -6,7 +6,7 @@ import style from "../../../styles/Util.module.scss"
 const FebMar2022Page = () => {
   return (
     <MonthlyLayout>
-      <h1>Månadens problem - Augusti och Maj 2022</h1>
+      <h1>Månadens problem - April och Maj 2022</h1>
       <p>Jag skapade den här kortleken för att kunna lära mig flaggan utantill! <a href="/monthly/aprmaj-2022/chall.apkg">Fil</a>.</p>
     </MonthlyLayout>
   )
