ec9ffc2347e14c1c4f6f32cd7e6d1497d161eb33
mateuszdrwal <drwal.mateusz@gmail.com>
2022-07-03 00:49:27 +0200

    service

--- src/pages/monthly/jul-2022/index.js
+++ src/pages/monthly/jul-2022/index.js
@@ -7,7 +7,7 @@ const FebMar2022Page = () => {
   return (
     <MonthlyLayout>
       <h1>Månadens problem - Juli 2022</h1>
-      <p>Några hackare från Statens Paketshanteringsverk har installerat en bakdörr på våran echo-server! Kan du förstå vad man behöver skicka för trafik för att aktivera bakdörren? <a href="/monthly/jul-2022/backdoor">Fil</a>.</p>
+      <p>Några hackare från Statens Pakethanteringsverk har installerat en bakdörr på våran echo-server! Kan du förstå vad man behöver skicka för trafik för att aktivera bakdörren? <a href="/monthly/jul-2022/backdoor">Fil</a>. Servern ligger på <code>35.228.72.74:31337</code>.</p>
     </MonthlyLayout>
   )
 }
